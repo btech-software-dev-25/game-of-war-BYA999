6cc9ab741dc16d9f9cae6201a1c31f12aaafa240
Bryan Alba Romualdo <bryanalbaromualdo@gmail.com>
2026-06-04 20:39:23 -0600

    Refactor Deck class to manage card collection

--- Deck.cs
+++ Deck.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace GameOfWar
 {
     public class Deck
@@ -13,49 +16,105 @@ namespace GameOfWar
             "Hearts", "Diamonds", "Clubs", "Spades"
         };
 
+        private List<Card> _cards;
 
-        // Create a public int property Count that returns the Count value from the private collection _cards
+        public int Count
+        {
+            get { return _cards.Count; }
+        }
 
+        public Deck(List<Card>? cards = null, bool isEmptyDeck = false)
+        {
+            if (cards != null && cards.Count > 0)
+            {
+                _cards = cards;
+                return;
+            }
 
-        // Create a private field _cards that is a List<Card>
+            _cards = new List<Card>();
 
+            if (!isEmptyDeck)
+            {
+                InitializeDeck();
+            }
+        }
 
-        // Create a public constructor that takes two parameter: a List<card> called cards and a boolean value called isEmptyDeck
-        // If cards is not null and has elements in it, assign it to _cards and be done
-        // If cards is null or empty:
-        //     _cards should be initialized as an empty List<Card>
-        //     InitializeDeck() should be called if and only if isEmptyDeck is false
+        private void InitializeDeck()
+        {
+            foreach (string suit in Suits)
+            {
+                for (int rank = 0; rank < RankNames.Length; rank++)
+                {
+                    _cards.Add(new Card(suit, rank));
+                }
+            }
+        }
 
+        public void Shuffle()
+        {
+            Random random = new Random();
 
-        // Create a private void method called InitializeDeck() which does the following:
-        // Use RankNames and Suits in nested loops to generate all 52 combinations of rank and suit and add them to _cards
+            for (int i = _cards.Count - 1; i > 0; i--)
+            {
+                int randomIndex = random.Next(i + 1);
 
+                Card temp = _cards[i];
+                _cards[i] = _cards[randomIndex];
+                _cards[randomIndex] = temp;
+            }
+        }
 
-        // Create a public void method called Shuffle() which shuffles (rearranges) the cards in _cards
+        public Card CardAtIndex(int index)
+        {
+            if (index < 0 || index >= _cards.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
 
+            return _cards[index];
+        }
 
-        // Create a public method CardAtIndex which takes an int parameter for the index of a card and
-        // returns the card at the index specified, or throws IndexOutOfRangeException if index is too large or too small
+        public Card PullCardAtIndex(int index)
+        {
+            Card card = CardAtIndex(index);
+            _cards.RemoveAt(index);
 
+            return card;
+        }
 
-        // Create a public method PullCardAtIndex which does exactly the same thing as CardAtIndex
-        // with the additional feature that it _removes_ the card from the deck
+        public List<Card> PullAllCards()
+        {
+            List<Card> cards = new List<Card>(_cards);
+            _cards.Clear();
 
+            return cards;
+        }
 
-        // Create a public method PullAllCards that returns a list of all of the cards in the deck
-        // and removes them all from the deck, leaving it empty
+        public void PushCard(Card card)
+        {
+            _cards.Add(card);
+        }
 
+        public void PushCards(List<Card> cards)
+        {
+            _cards.AddRange(cards);
+        }
 
-        // Create a public method PushCard that accepts a Card as a parameter and adds it to _cards
+        public List<Card> Deal(int numberOfCards)
+        {
+            if (numberOfCards > _cards.Count)
+            {
+                numberOfCards = _cards.Count;
+            }
 
+            List<Card> dealtCards = new List<Card>();
 
-        // Create a public method PushCards that accepts a List<Card> as a parameter and adds the list to _cards
-        // Be sure to use AddRange and not Add
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                dealtCards.Add(PullCardAtIndex(0));
+            }
 
-
-        // Create a public method Deal that accepts an integer representing the number of cards to deal
-        // and then removes that many cards from the deck, returning them as a List<Card>
-        // Be sure to check the size of _cards against the number of cards requested so you don't go out
-        // of bounds
+            return dealtCards;
+        }
     }
 }

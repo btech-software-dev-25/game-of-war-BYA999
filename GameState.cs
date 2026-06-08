be032a42494faba52305e926447e7955490a441a
Bryan Alba Romualdo <bryanalbaromualdo@gmail.com>
2026-06-07 18:41:07 -0600

    Add GameState class with deck and winner properties
    Add GameState class with deck and winner properties

--- GameState.cs
+++ GameState.cs
@@ -1,1 +1,25 @@
 
+namespace GameOfWar
+{
+    public class GameState
+    {
+        public Deck CardDeck { get; set; }
+
+        public Deck PlayerDeck { get; set; }
+
+        public Deck ComputerDeck { get; set; }
+
+        public Deck TableDeck { get; set; }
+
+        public string Winner { get; set; }
+
+        public GameState()
+        {
+            Winner = "";
+            CardDeck = new Deck();
+            PlayerDeck = new Deck(isEmptyDeck: true);
+            ComputerDeck = new Deck(isEmptyDeck: true);
+            TableDeck = new Deck(isEmptyDeck: true);
+        }
+    }
+}
